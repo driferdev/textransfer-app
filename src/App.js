9c0f667a23dcb8df1f02c1e0b0f2df2fd62f6621
Jose Rodriguez <jose.guillen@Dynata.com>
2020-03-13 22:08:38 -0500

    adding not found page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Room from './components/Room/Room';
+import NotFound from './components/NotFound/NotFound';
 import './App.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home}/>
-      <Route path="/room/:name" exact component={Room}/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/room/:name" exact component={Room}/>
+        <Route path="*" exact={true} component={NotFound}/>
+      </Switch>
     </BrowserRouter>
   );
 }
